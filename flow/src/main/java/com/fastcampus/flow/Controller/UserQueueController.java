32b6662fe376ddd320c15f319a199d16c875f976
suhokym <suhokym@gmail.com>
2026-03-03 22:08:55 +0900

    대기열 이탈 완성

--- flow/src/main/java/com/fastcampus/flow/Controller/UserQueueController.java
+++ flow/src/main/java/com/fastcampus/flow/Controller/UserQueueController.java
@@ -8,8 +8,12 @@ import com.fastcampus.flow.exception.ApplicationException;
 import com.fastcampus.flow.service.UserQueueService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/api/v1/queue")
@@ -19,28 +23,46 @@ public class UserQueueController {
     private final UserQueueService userQueueService;
 
 
-        @PostMapping("")
-        public Mono<RegisterUserResponse> registerUser(@RequestParam(name = "queue" , defaultValue = "default") String queue,
-                                                       @RequestParam(name = "user_id") Long userId){
-            return userQueueService.registerWaitQueue(queue, userId)
-                    .map(RegisterUserResponse::new);
-        }
+    @PostMapping("")
+    public Mono<RegisterUserResponse> registerUser(@RequestParam(name = "queue", defaultValue = "default") String queue,
+                                                   @RequestParam(name = "user_id") Long userId) {
+        return userQueueService.registerWaitQueue(queue, userId)
+                .map(RegisterUserResponse::new);
+    }
 
-        @PostMapping("allow")
-        public Mono<AllowUserResponse> allowUser(@RequestParam(name = "queue" , defaultValue = "default") String queue,
-                                                 @RequestParam(name = "count") Long count){
-                return userQueueService.allowUser(queue, count)
-                        .map(allowed -> new AllowUserResponse(count, allowed));
-        }
-        @GetMapping("/allowed")
-        public Mono<AllowedUserResponse> isAllowedUser(@RequestParam(name = "queue" , defaultValue = "default") String queue,
-                                     @RequestParam(name = "user_id") Long userId){
-           return userQueueService.isAllowed(queue, userId).map(AllowedUserResponse::new);
-        }
+    @PostMapping("allow")
+    public Mono<AllowUserResponse> allowUser(@RequestParam(name = "queue", defaultValue = "default") String queue,
+                                             @RequestParam(name = "count") Long count) {
+        return userQueueService.allowUser(queue, count)
+                .map(allowed -> new AllowUserResponse(count, allowed));
+    }
+
+    @GetMapping("/allowed")
+    public Mono<AllowedUserResponse> isAllowedUser(@RequestParam(name = "queue", defaultValue = "default") String queue,
+                                                   @RequestParam(name = "user_id") Long userId) {
+        return userQueueService.isAllowed(queue, userId).map(AllowedUserResponse::new);
+    }
+
 
     @GetMapping("/rank")
-    public Mono<RankNumberResponse> getRankUser(@RequestParam(name = "queue" , defaultValue = "default") String queue,
-                                                                        @RequestParam(name = "user_id") Long userId){
+    public Mono<RankNumberResponse> getRankUser(@RequestParam(name = "queue", defaultValue = "default") String queue,
+                                                @RequestParam(name = "user_id") Long userId) {
         return userQueueService.getRank(queue, userId).map(RankNumberResponse::new);
+    }
+
+    @GetMapping("/touch")
+    Mono<?> touch(@RequestParam(name = "queue", defaultValue = "default") String queue,
+                  @RequestParam(name = "user_id") Long userId,
+                  ServerWebExchange exchange) {
+        return Mono.defer(() -> userQueueService.generateToken(queue, userId))
+                .map(token -> {
+                    exchange.getResponse().addCookie(
+                            ResponseCookie.from("user-queue-%s-token".formatted(queue), token)
+                                    .maxAge(Duration.ofSeconds(300))
+                                    .path("/")
+                                    .build()
+                    );
+                    return token;
+                });
     }
 }
--- flow/src/main/java/com/fastcampus/flow/service/UserQueueService.java
+++ flow/src/main/java/com/fastcampus/flow/service/UserQueueService.java
@@ -1,14 +1,25 @@
 package com.fastcampus.flow.service;
 
 import com.fastcampus.flow.exception.ErrorCode;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.yaml.snakeyaml.util.Tuple;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.time.Instant;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserQueueService {
@@ -17,9 +28,13 @@ public class UserQueueService {
 
     private final String USER_QUEUE_WAIT_KEY = "users:queue:%s:wait";
 
+    private final String USER_QUEUE_WAIT_KEY_FOR_SCAN = "users:queue:*:wait";
+
     private final String USER_QUEUE_PROCEED_KEY = "users:queue:%s:proceed";
 
-    public Mono<Long> registerWaitQueue(final String queue, final Long userId){
+    @Value("${scheduler.enabled}")
+    private Boolean scheduling = false;
+    public Mono<Long> registerWaitQueue(final String queue, final Long userId) {
         //redis sortedset
         // - key : userId
         // - value: unix timestamp
@@ -29,7 +44,7 @@ public class UserQueueService {
                 .filter(i -> i)
                 .switchIfEmpty(Mono.error(ErrorCode.QUEUE_ALREADY_REGISERED_USER.build()))
                 .flatMap(i -> reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString()))
-                .map(( i -> i >= 0 ? i+1 : i));
+                .map((i -> i >= 0 ? i + 1 : i));
 
     }
 
@@ -45,19 +60,68 @@ public class UserQueueService {
                         .add(USER_QUEUE_PROCEED_KEY.formatted(queue), member.getValue(), Instant.now().getEpochSecond()))
                 .count();
     }
+
     //진입이 가능한 상태인지 조회
     public Mono<Boolean> isAllowed(final String queue, final Long userId) {
-         return reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_PROCEED_KEY.formatted(queue), userId.toString())
+        return reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_PROCEED_KEY.formatted(queue), userId.toString())
                 .defaultIfEmpty(-1L)
                 .map(rank -> rank >= 0);
 
     }
+
+    //진입이 가능한 상태인지 조회
+    public Mono<Boolean> isAllowedByToken(final String queue, final Long userId, final String token) {
+        return this.generateToken(queue, userId)
+                .filter(gen -> gen.equalsIgnoreCase(token))
+                .map(i -> true)
+                .defaultIfEmpty(false);
+
+    }
+
 
     public Mono<Long> getRank(final String queue, final Long userId) {
         return reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString())
                 .defaultIfEmpty(-1L)
                 .map(rank -> rank >= 0 ? rank + 1 : rank);
 
+
+    }
+
+    public Mono<String> generateToken(final String queue, final Long userId){
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+            var input = "user-queue-%s-%d".formatted(queue, userId);
+            byte[] encodedHash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder();
+            for(byte aByte : encodedHash){
+                hexString.append(String.format("%02x", aByte));
+            }
+            return Mono.just(hexString.toString());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Scheduled(initialDelay = 5000, fixedDelay = 10000)
+    public void scheduleAllowed() {
+        if (!scheduling) {
+            log.info("passed scheduling");
+            return;
+        }
+        log.info("called scheduling...");
+
+        var maxAllowUserCount = 3L;
+
+        reactiveRedisTemplate.scan(ScanOptions.scanOptions()
+                        .match(USER_QUEUE_WAIT_KEY_FOR_SCAN)
+                        .count(100)
+                        .build())
+                .map(key -> key.split(":")[2])
+                .flatMap(queue -> allowUser(queue, maxAllowUserCount).map(allowed -> Tuples.of(queue, allowed)))
+                .doOnNext(tuple -> log.info("Tried %d and allowed %d members of %s queue".formatted(maxAllowUserCount, tuple.getT2(),tuple.getT1())))
+                .subscribe();
 
     }
 }
